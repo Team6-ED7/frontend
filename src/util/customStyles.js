8fe91e47dae25cc9aa79294956e2a62a8a77a33c
Jmacosta <jmab2k@gmail.com>
2024-07-30 22:06:23 +0200

    Feat:Add custom styles for form fields

---
+++ src/util/customStyles.js
@@ -0,0 +1,28 @@
+export const inputStyleConfig = {
+  inputWrapper:
+    ' border-1 border-primary text- border-solid bg-white text-default-300 font-poppins',
+  label: 'text-secondary',
+};
+
+export const selectStyleConfig = { trigger: 'border-1 border-primary' };
+
+export const buttonStyleConfig = ' font-medium font-poppins';
+
+export const radioGroupStyleConfig = {
+  label: 'mx-auto',
+};
+export const radioStyleConfig = {
+  wrapper: ' border-secondary ',
+};
+
+export const checkBoxStyleConfig = {
+  wrapper: 'border-2 border-secondary border-solid text-foreground',
+  base: 'data-[selected=true]:border-primary',
+  label: 'font-poppins',
+};
+
+export const tableStyleConfig = {
+  thead: 'font-poppins font-medium  text-tertiary',
+  th: 'text-center text-md',
+  td: '',
+};
