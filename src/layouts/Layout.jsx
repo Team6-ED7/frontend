5b489e0008c428b424bf47c4be3bc67d943958b0
Jhoandris B <jrbaronp@gmail.com>
2024-08-03 11:47:48 -0500

    feature/dashboard  protected layout

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,16 +1,9 @@
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet, } from 'react-router-dom';
 import Footer from '../components/common/Footer';
 import Header from '../components/common/Header';
 
 const Layout = () => {
-  //TODO Cambiar por autenticación y contex
-  const user = localStorage.getItem('user');
-  const navigate = useNavigate();
-  // useEffect(() => {
-  //    if (!user) {
-  //      navigate('/login');
-  //    }
-  //  }, [navigate, user]);
+
   return (
     <div className="h-screen grid grid-rows-[auto_1fr_auto]">
       <Header />
