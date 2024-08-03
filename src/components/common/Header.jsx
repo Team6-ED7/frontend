03c16fe7c9b16e79eb9de7c4ae3935d9f05dcbc2
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-08-03 14:46:10 +0200

    Feat: Header section with logo

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,6 +1,13 @@
+import { Link } from 'react-router-dom';
+import LogoLandscape from './LogoLandscape';
 
 export const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="px-5 py-5">
+      <Link to="/">
+        <LogoLandscape />
+      </Link>
+    </div>
+  );
+};
+export default Header;
