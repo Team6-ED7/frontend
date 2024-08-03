309d656cd75f20c99bfb5604e493ded4237f63a7
Jhoandris B <jrbaronp@gmail.com>
2024-08-03 10:01:17 -0500

    hoty fixed  link HOme page

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Layout from '../layouts/Layout';
 import ErrorPage from '../pages/error-page';
-import Home from '../pages/Home/Home';
+import Home from '../pages/Home';
 import Register from '../pages/Register';
 import Login from '../pages/Login';
 export const router = createBrowserRouter([
