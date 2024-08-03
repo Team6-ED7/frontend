a90adbf7ef6559d042c6ec7c30b2b7fc7d3a3a12
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-08-03 14:53:01 +0200

    Feat: homepage

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,44 @@
+import { Button } from '@nextui-org/react';
+import { IconLogin2 as LoginIcon } from '@tabler/icons-react';
+import { Link } from 'react-router-dom';
+import H3title from '../../components/common/H3Title';
+import LogoPortrait from '../../components/common/LogoPortrait';
+import { buttonStyleConfig } from '../../util/customStyles';
+
+const Home = () => {
+  return (
+    <main className="bg-[url('/src/assets/background-home.jpg')] bg-bottom bg-no-repeat bg-cover max-h-screen flex flex-col w-screen items-center justify-center ">
+      <section className="max-w-4xl px-10  py-10 flex flex-col items-center gap-10 rounded-md bg-white bg-opacity-90">
+        <header>
+          <LogoPortrait />
+        </header>
+        <main>
+          <H3title>Encuentra tu espacio ideal de coworking al instante</H3title>
+          <p>
+            Reserva el espacio perfecto para trabajar y colaborar en segundos.
+            Con nuestro sistema intuitivo y fácil de usar, descubre y reserva
+            oficinas, salas de reuniones y puestos de trabajo en las mejores
+            ubicaciones. ¡Optimiza tu productividad y conecta con una comunidad
+            de profesionales!
+          </p>
+          <p>¡Reserva ahora y empieza a trabajar!</p>
+        </main>
+        <footer className=" flex justify-center">
+          <Link to="register">
+            <Button
+              color="primary"
+              variant="solid"
+              size="lg"
+              endContent={<LoginIcon />}
+              className={buttonStyleConfig}
+            >
+              Login / Register
+            </Button>
+          </Link>
+        </footer>
+      </section>
+    </main>
+  );
+};
+
+export default Home;
