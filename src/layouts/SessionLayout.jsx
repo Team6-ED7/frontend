8ccbd36a81bdec7041593c11257d307f05e90526
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-08-06 21:53:56 +0200

    FIX: change background panel opacity and color

--- src/layouts/SessionLayout.jsx
+++ src/layouts/SessionLayout.jsx
@@ -1,16 +1,14 @@
-import LogoPortrait from "../components/common/LogoPortrait"
+import LogoPortrait from '../components/common/LogoPortrait';
 
 export const SessionLayout = ({ children }) => {
   return (
     <main className="bg-[url('/src/assets/background.svg')] flex flex-col w-screen items-center ">
-      <section className="w-7xl px-10  py-10 flex flex-col items-center gap-10">
+      <section className="w-7xl px-10  py-10 flex flex-col items-center gap-5">
         <LogoPortrait />
-        <div className="max-w-4xl p-10 rounded-md bg-black bg-opacity-10 flex flex-col gap-5">
+        <div className="max-w-4xl p-10 rounded-md bg-white bg-opacity-80 flex flex-col gap-5">
           {children}
-
         </div>
       </section>
-
     </main>
-  )
-}
+  );
+};
