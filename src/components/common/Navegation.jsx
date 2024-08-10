c673440561a159275b2b7238037e05618e659355
Jhoandris B <jrbaronp@gmail.com>
2024-08-10 14:27:02 -0500

    Change A for LINK

--- src/components/common/Navegation.jsx
+++ src/components/common/Navegation.jsx
@@ -1,36 +1,35 @@
-import {useState } from 'react';
+import { useState } from 'react';
 import { HiMenu } from "react-icons/hi";
 import LogoPortrait from '../../components/common/LogoPortrait';
-import { Button} from '@nextui-org/react';
+import { Button } from '@nextui-org/react';
 import { buttonStyleConfig } from '../../util/customStyles';
 import { Link } from 'react-router-dom';
-import Home from '../../pages/Home';
 
 
 
 const Navegation = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-   
+
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
     };
 
 
-    return ( 
+    return (
         <header className='w-full fixed top-0 left-0 right-0'>
             <nav className='py-4 md:px-8 px-4 bg-white ${isSticky ? "sticky top-0 right-0 left-0 bg-white" : "" } '>
                 <div className='flex items-center justify-between '>
                     <Link to={"/"} className='cursor-pointer flex items-center'>
-                        < LogoPortrait height={60}/>
+                        < LogoPortrait height={60} />
                     </Link>
 
                     {/*para dispositivos mas grandes*/}
 
-                    <div className='lg:flex items-center gap-3 hidden text-stone-950'>  
-                        <a href='/' className='block hover:underline py-2 px-4'>Encuentra una ubicación</a>
-                        <a href='/' className='block hover:underline py-2 px-4'>Soluciones</a>
-                        <a href='/' className='block hover:underline py-2 px-4'>Enterprise</a>
-                        <a href='/' className='block hover:underline py-2 px-4'>Inicio de Sesion de Miembros </a>
+                    <div className='lg:flex items-center gap-3 hidden text-stone-950'>
+                        <Link to='/' className='block hover:underline py-2 px-4'>Encuentra una ubicación</Link>
+                        <Link to='/' className='block hover:underline py-2 px-4'>Soluciones</Link>
+                        <Link to='/' className='block hover:underline py-2 px-4'>Enterprise</Link>
+                        <Link to='/' className='block hover:underline py-2 px-4'>Inicio de Sesion de Miembros </Link>
                     </div>
                     {/*Boton de uneteya!*/}
                     <div className='lg:block hidden'>
@@ -39,12 +38,12 @@ const Navegation = () => {
 
                     {/*boton del menu para dispositivos pequenos*/}
                     <button onClick={toggleMenu} className='lg:hidden text-black text-3x1'>
-                        <HiMenu/>
+                        <HiMenu />
                     </button>
-                    
+
                 </div>
                 {/* nav para telefono*/}
-                {isMenuOpen &&(
+                {isMenuOpen && (
                     <div className='mt-4 bg-white-400 text-black rounded py-4'>
                         <a href='/' className='block hover:text-gray-500 py-2 px-4 text-center'>Encuentra una ubicación</a>
                         <a href='/' className='block hover:text-gray-500 py-2 px-4 text-center'>Soluciones</a>
